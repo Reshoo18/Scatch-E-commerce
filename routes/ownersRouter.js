56040dde0a8635cee349acdc03f42dda5e063288
Reshoo-18 <reshooranjan7@gmail.com>
2025-10-04 23:27:32 +0530

    api testing created user

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -12,12 +12,26 @@ router.get("/",function(req,res){
 });
 
 
- console.log(process.env.NODE_ENV)
+ //console.log(process.env.NODE_ENV)
 if(process.env.NODE_ENV==="development"){
 
 
-router.post("/create",function(req,res){
-    res.send("hey its working ");
+router.post("/create",async function(req,res){
+
+  let owners=  await ownerModel.find();
+  if(owners.length>0) {return res.send(503).send("you have no permission to create a new owner")
+  }
+
+let {fullname,email,password}=req.body;
+ let createdOwner= await ownerModel.create({
+    fullname,
+    email,
+    password,
+    gstin,
+  })
+res.status(201).send(createdOwner)
+
+
 });
 }
 
